194e5a5ac37d2788c13fc08d9d8c379740190b90
Fien-Dora <fiendora5@gmail.com>
2023-07-27 15:58:54 +0100

    sites

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,14 @@
 import Summary from "../components/summary.js";
 import Statistics from "../components/sites/Statistics.js";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-
 
 const Home = () => {
-  return ( 
+  return (
     <>
-      <Container fluid className="min-vh-100 background: #F0F2F5 ">
-        <Row>
-          <Col>
-            <Summary/>
+      <Summary />
 
-            <Statistics/>
-          </Col>
-        </Row>
-      </Container>
+      <Statistics />
     </>
-   );
-}
- 
+  );
+};
+
 export default Home;
