7a4b076edf6f085d9fe272ccee699f95bce6e54b
MbLespa <mbahlesky2@gmail.com>
2023-07-27 15:13:03 +0100

    lil changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,8 +4,8 @@ import search from "../assets/img/search.png";
 
 const Sidebar = () => {
   return (
-    <>
-      <div className="bg-white sidebar p-2 m-0">
+    <div className="d-flex">
+      <div className="bg-white sidebar p-2 m-0" style={{minWidth: '200px'}}>
         <div className="row g-0 m-0 p-1 justify-content-center">
           <div className="col-4 ">
             <img src="/logo.jpg" alt="logoFromPublic" className="siderLogo" />
@@ -59,8 +59,9 @@ const Sidebar = () => {
           </a>
         </div>
       </div>
+
       <Outlet />
-    </>
+    </div>
   );
 };
 
